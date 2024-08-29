b47124fc3ab48e2b9dc9fa452f21460145c22244
xuzhiping <xuzhiping@xd.com>
2024-08-29 19:24:30 +0800

    fix

--- src/main/java/com/example/demo/listener/MyCustomSpringApplicationRunListener.java
+++ src/main/java/com/example/demo/listener/MyCustomSpringApplicationRunListener.java
@@ -2,12 +2,19 @@ package com.example.demo.listener;
 
 import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 public class MyCustomSpringApplicationRunListener implements SpringApplicationRunListener {
 
     @Override
     public void starting(ConfigurableBootstrapContext bootstrapContext) {
         System.out.println("MyCustomSpringApplicationRunListener starting");
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableBootstrapContext bootstrapContext,
+                             ConfigurableEnvironment environment) {
+        System.out.println("MyCustomSpringApplicationRunListener environmentPrepared");
     }
 
 }
