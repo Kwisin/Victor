5eee06d680c3109d5f574c5652ae7fb1138da215
kwisin <597729905@qq.com>
2024-09-14 23:22:18 +0800

    feat:新增算法題

---
+++ src/main/java/com/example/demo/study/demo2409/Demo0915.java
@@ -0,0 +1,21 @@
+package com.example.demo.study.demo2409;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Demo0915 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+// 2390
+
+
+// 5
+
+
+
+// 10
