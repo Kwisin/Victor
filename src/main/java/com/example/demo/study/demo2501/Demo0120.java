928dcd4bdd8ed4761b5a38275afc3fae842c527e
xuzhiping <xuzhiping@xd.com>
2025-01-20 18:11:21 +0800

    feat: 2025-01-20算法题

---
+++ src/main/java/com/example/demo/study/demo2501/Demo0120.java
@@ -0,0 +1,15 @@
+package com.example.demo.study.demo2501;
+
+
+import java.util.Arrays;
+
+public class Demo0120 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+}
+
+//
