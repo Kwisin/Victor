852987d317c6ee7784578488518726d3d85d31c5
xuzhiping <xuzhiping@xd.com>
2025-02-08 19:28:12 +0800

    feat: 2025-02-08算法题

--- src/main/java/com/example/demo/study/demo2502/Demo0208.java
+++ src/main/java/com/example/demo/study/demo2502/Demo0208.java
@@ -3,10 +3,12 @@ package com.example.demo.study.demo2502;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Demo0208 {
 
     public static void main(String[] args) {
+        boolean urr = new robot().robot("URR", new int[][]{{2, 2}}, 3, 2);
         System.out.println();
     }
 
@@ -201,7 +203,7 @@ class fractionToDecimal {
 class validNumber {
 
     public boolean validNumber(String s) {
-        String s1 = s.toLowerCase().replace(" ","");
+        String s1 = s.toLowerCase().replace(" ", "");
         HashMap<Character, ArrayList<Integer>> characterPositionHashMap = new HashMap<>();
         for (int i = 0; i < s1.length(); i++) {
             char c = s1.charAt(i);
@@ -246,5 +248,59 @@ class validNumber {
 
         }
         return true;
+    }
+}
+
+// LCP 03. 机器人大冒险
+/*
+力扣团队买了一个可编程机器人，机器人初始位置在原点(0, 0)。
+小伙伴事先给机器人输入一串指令command，机器人就会无限循环这条指令的步骤进行移动。指令有两种：
+
+U: 向y轴正方向移动一格
+R: 向x轴正方向移动一格。
+不幸的是，在 xy 平面上还有一些障碍物，他们的坐标用obstacles表示。机器人一旦碰到障碍物就会被损毁。
+
+给定终点坐标(x, y)，返回机器人能否完好地到达终点。如果能，返回true；否则返回false。
+
+
+
+示例 1：
+输入：command = "URR", obstacles = [], x = 3, y = 2
+输出：true
+解释：U(0, 1) -> R(1, 1) -> R(2, 1) -> U(2, 2) -> R(3, 2)。
+示例 2：
+输入：command = "URR", obstacles = [[2, 2]], x = 3, y = 2
+输出：false
+解释：机器人在到达终点前会碰到(2, 2)的障碍物。
+示例 3：
+输入：command = "URR", obstacles = [[4, 2]], x = 3, y = 2
+输出：true
+解释：到达终点后，再碰到障碍物也不影响返回结果。
+ */
+
+class robot {
+    public boolean robot(String command, int[][] obstacles, int x, int y) {
+        HashSet<String> stringHashSet = new HashSet<>();
+        for (int[] item : obstacles) {
+            stringHashSet.add(item[0] + "-" + item[1]);
+        }
+        int xStart = 0, yStart = 0, curr = 0;
+        if (stringHashSet.contains(xStart + "-" + yStart)) {
+            return false;
+        }
+        while (xStart < x || yStart < y) {
+            char c = command.charAt(curr % command.length());
+            if (c == 'U') {
+                yStart++;
+            } else if (c == 'R') {
+                xStart++;
+            }
+            if (stringHashSet.contains(xStart + "-" + yStart)) {
+                return false;
+            }
+            curr++;
+        }
+
+        return xStart == x && yStart == y;
     }
 }
