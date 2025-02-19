7dafda20df334edc8476ab36f2a20213a77e0568
xuzhiping <xuzhiping@xd.com>
2025-02-19 15:02:55 +0800

    feat: 2025-02-14算法题

--- src/main/java/com/example/demo/study/demo2502/Demo0214.java
+++ src/main/java/com/example/demo/study/demo2502/Demo0214.java
@@ -6,7 +6,8 @@ import java.util.Arrays;
 public class Demo0214 {
 
     public static void main(String[] args) {
-        int rob = new rob().rob(new int[]{1, 2, 3, 1, 1, 1});
+        int rob = new rob().rob(new int[]{
+                183, 219, 57, 193, 94, 233, 202, 154, 65, 240, 97, 234, 100, 249, 186, 66, 90, 238, 168, 128, 177, 235, 50, 81, 185, 165, 217, 207, 88, 80, 112, 78, 135, 62, 228, 247, 211});
         System.out.println();
     }
 
@@ -26,7 +27,7 @@ BEGIN
 END
  */
 
-// 213. 打家劫舍 II
+// 213. 打家劫舍 II todo
 /*
 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。
 同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
@@ -61,9 +62,6 @@ s1  [1[1],2[0],4[1],3[0],5[1],5[1]]
 class rob {
     public int rob(int[] nums) {
         int length = nums.length;
-        if (length <= 2) {
-            return 0;
-        }
 
         return dfs(nums, new int[length], 0, 0);
     }
@@ -74,10 +72,27 @@ class rob {
         }
         int notValue = dfs(nums, used, currPos + 1, currVal);
         int yesValue = 0;
-        if (currPos == 0 || used[currPos - 1] == 0 && !(used[0] == 1 && currPos == nums.length - 1)) {
+        if ((currPos == 0 || used[currPos - 1] == 0) && !(used[0] == 1 && currPos == nums.length - 1)) {
             used[currPos] = 1;
             yesValue = dfs(nums, used, currPos + 1, currVal + nums[currPos]);
+            used[currPos] = 0;
         }
         return Math.max(notValue, yesValue);
+    }
+
+    public int dpSolution(int[] nums) {
+        int length = nums.length;
+        int[][] dp = new int[2][length];
+        dp[0][0] = 0;
+        dp[1][0] = nums[0];
+        for (int i = 1; i < length; i++) {
+            dp[1][i] = Math.max(dp[0][i - 1] + nums[i], dp[1][i - 1]);
+            dp[0][i] = Math.max(dp[1][i - 1], dp[0][i - 1]);
+        }
+
+
+        return 0;
+
+
     }
 }
