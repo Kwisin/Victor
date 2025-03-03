9c9c5cd1203f4497af050dcb6c429590619e0ccb
xuzhiping <xuzhiping@xd.com>
2025-03-03 13:17:21 +0800

    feat: 2025-03-03算法题

---
+++ src/main/java/com/example/demo/study/demo2503/Demo0303.java
@@ -0,0 +1,79 @@
+package com.example.demo.study.demo2503;
+
+
+import java.util.List;
+
+public class Demo0303 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
+
+// 229. 多数元素 II
+/*
+给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+
+
+示例 1：
+
+输入：nums = [3,2,3]
+输出：[3]
+示例 2：
+
+输入：nums = [1]
+输出：[1]
+示例 3：
+
+输入：nums = [1,2]
+输出：[1,2]
+ */
+class majorityElement {
+    public List<Integer> majorityElement(int[] nums) {
+        return null;
+    }
+}
+
+// 221. 最大正方形
+/*
+在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+
+输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+输出：4
+
+输入：matrix = [["0","1"],["1","0"]]
+输出：1
+示例 3：
+
+输入：matrix = [["0"]]
+输出：0
+ */
+class maximalSquare {
+    public int maximalSquare(char[][] matrix) {
+        return 0;
+    }
+}
+
+// 227. 基本计算器 II
+/*
+给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+整数除法仅保留整数部分。
+你可以假设给定的表达式总是有效的。所有中间结果将在 [-231, 231 - 1] 的范围内。
+注意：不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。
+
+示例 1：
+输入：s = "3+2*2"
+输出：7
+示例 2：
+输入：s = " 3/2 "
+输出：1
+示例 3：
+输入：s = " 3+5 / 2 "
+输出：5
+ */
+class calculate {
+    public int calculate(String s) {
+        return 0;
+    }
+}
