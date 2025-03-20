3186a6640b9fe850fb621125c54132782ac7c541
xuzhiping <xuzhiping@xd.com>
2025-03-20 15:50:42 +0800

    feat: go pkg

--- golang_pkg/main.go
+++ golang_pkg/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/samber/lo"
+)
 
 func main() {
 	strings := make([]string, 0)
 	strings = append(strings, "", "1")
 	fmt.Println(strings[1])
-
+	lo.Contains(strings, "")
 	fmt.Println("222")
 }
