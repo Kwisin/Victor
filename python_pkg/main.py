88814d2048bb90b90d9807e4911e7990a1126801
kwisin <597729905@qq.com>
2025-03-23 23:15:32 +0800

    feat:python pkg

--- python_pkg/main.py
+++ python_pkg/main.py
@@ -1,8 +1,12 @@
-from tensorflow.core.config import flags
-from pandas.core import common
-from torch.nn import functional
 
 if __name__ == '__main__':
-    common.flatten()
-    print("Hello, World!")
-    print("Hello, World!")
+    i = 1
+    j = 1.08
+    list = [1, 2, 3, "", 1.09]
+    tuple = (1, 2, "5", 1.09)
+    dic = {"": "1"}
+    set = {1, 2, 3, 5, 3}
+    print(list[1])
+    print(tuple[2])
+    print(dic[""])
+    print(set[1])
