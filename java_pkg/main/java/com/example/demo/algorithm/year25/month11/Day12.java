65ff74aef63ba4fa899f1d403052dbdf3565a6fd
xuzhiping@xd.com <xuzhiping@xd.com>
2025-11-12 13:00:50 +0800

    feat: algorithm 2025-11-12

---
+++ java_pkg/main/java/com/example/demo/algorithm/year25/month11/Day12.java
@@ -0,0 +1,43 @@
+package com.example.demo.algorithm.year25.month11;
+
+public class Day12 {
+    public static void main(String[] args) {
+
+    }
+    // 164
+    //    给定一个无序的数组 nums，返回 数组在排序之后，相邻元素之间最大的差值 。如果数组元素个数小于 2，则返回 0 。
+    //
+    //    您必须编写一个在「线性时间」内运行并使用「线性额外空间」的算法。
+    //
+    //
+    //
+    //    示例 1:
+    //
+    //    输入: nums = [3,6,9,1]
+    //    输出: 3
+    //    解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+    //    示例 2:
+    //
+    //    输入: nums = [10]
+    //    输出: 0
+    //    解释: 数组元素个数小于 2，因此返回 0。
+    public int maximumGap(int[] nums) {
+        return 0;
+    }
+
+    /*
+    给你一个整数数组 nums 和两个整数 indexDiff 和 valueDiff 。
+
+    找出满足下述条件的下标对 (i, j)：
+    i != j,
+    abs(i - j) <= indexDiff
+    abs(nums[i] - nums[j]) <= valueDiff
+    如果存在，返回 true ；否则，返回 false 。
+     */
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        return false;
+    }
+}
+
+
